109afaf557314cd78f2cf14a64a308b544f83ed2
Rafael <rafaelcavalcante330@gmail.com>
2025-09-08 20:59:00 -0300

    Função de somar melhorada
    O codigo agora mostra o resultado final da soma

--- src/Main.java
+++ src/Main.java
@@ -7,5 +7,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Olá, GitHub!");
 
+        int result = somar(5, 6);
+        System.out.println(result);
+
     }
 }
